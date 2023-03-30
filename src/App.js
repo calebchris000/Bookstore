859f565c162e193118c1d0d87502f06224ab74d7
calebchris000 <calebchris000@gmail.com>
2023-03-30 14:59:34 +0100

    Initialize project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import Home from './components/Home';
+import Categories from './components/Categories';
+import Books from './components/Books';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route index path="/" element={<Home />} />
+      <Route path="/books" element={<Books />} />
+      <Route path="/categories" element={<Categories />} />
+    </Route>,
+  ),
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
